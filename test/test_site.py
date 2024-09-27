2a183507deb68b2b1f280db7f3fb946a21e6f6a6
KupeTT <danik.iz.2001@mail.ru>
2024-09-27 18:34:19 +0300

    Tests ready

---
+++ test/test_site.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+
+import time
+from pages.homepage import HomePage
+from pages.product import ProductPage
+
+def test_open_s6(browser):
+
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.click_galaxy_s6()
+    product_page = ProductPage(browser)
+    product_page.check_title_is('Samsung galaxy s6')
+
+
+def test_two_monitors(browser):
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.click_monitor()
+    time.sleep(3)
+    homepage.check_product_count(2)
+
+
